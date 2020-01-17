c605ee3220bffd8cb1400a008564aba8f7e497cd
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-01-17 13:32:08 -0800

    Use KeyAddr

--- GamingMode.cpp
+++ GamingMode.cpp
@@ -8,19 +8,19 @@ namespace kaleidoscope {
   void GamingMode::setGamingColors() {
     ::LEDControl.set_all_leds_to(CRGB(0, 0, 170));
 
-    ::LEDControl.setCrgbAt(1, 2, CRGB(170, 0, 0)); // w
-    ::LEDControl.setCrgbAt(2, 1, CRGB(170, 0, 0)); // a
-    ::LEDControl.setCrgbAt(2, 2, CRGB(170, 0, 0)); // s
-    ::LEDControl.setCrgbAt(2, 3, CRGB(170, 0, 0)); // d
+    ::LEDControl.setCrgbAt(KeyAddr(1, 2), CRGB(170, 0, 0)); // w
+    ::LEDControl.setCrgbAt(KeyAddr(2, 1), CRGB(170, 0, 0)); // a
+    ::LEDControl.setCrgbAt(KeyAddr(2, 2), CRGB(170, 0, 0)); // s
+    ::LEDControl.setCrgbAt(KeyAddr(2, 3), CRGB(170, 0, 0)); // d
 
-    ::LEDControl.setCrgbAt(3, 7, CRGB(203, 101, 0)); // shift
-    ::LEDControl.setCrgbAt(2, 0, CRGB(203, 101, 0)); // shift (pgup)
+    ::LEDControl.setCrgbAt(KeyAddr(3, 7), CRGB(203, 101, 0)); // shift
+    ::LEDControl.setCrgbAt(KeyAddr(2, 0), CRGB(203, 101, 0)); // shift (pgup)
 
-    ::LEDControl.setCrgbAt(0, 7, CRGB(0, 160, 0)); // control
-    ::LEDControl.setCrgbAt(3, 0, CRGB(0, 160, 0)); // control (pgdn)
+    ::LEDControl.setCrgbAt(KeyAddr(0, 7), CRGB(0, 160, 0)); // control
+    ::LEDControl.setCrgbAt(KeyAddr(3, 0), CRGB(0, 160, 0)); // control (pgdn)
 
-    ::LEDControl.setCrgbAt(1, 7, CRGB(0, 160, 160)); // space (backspace)
-    ::LEDControl.setCrgbAt(2, 7, CRGB(0, 160, 160)); // alt (cmd)
+    ::LEDControl.setCrgbAt(KeyAddr(1, 7), CRGB(0, 160, 160)); // space (backspace)
+    ::LEDControl.setCrgbAt(KeyAddr(2, 7), CRGB(0, 160, 160)); // alt (cmd)
   }
 
   EventHandlerResult GamingMode::beforeReportingState() {
